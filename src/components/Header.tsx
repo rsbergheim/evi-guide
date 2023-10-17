3925f8e6f05a9a36b7f9a261b2131ba5158dbaa2
Roger Schei Bergheim <rsbergheim@gmail.com>
2023-10-17 23:06:56 +0200

    Legger til styling for header, footer og hjem-side

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,33 @@ import React from 'react';
 
 const Header: React.FC = () => {
     return (
-        <div>
-            <img src="path_to_your_logo" alt="Logo" />
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/turer">Turer</a></li>
-                    <li><a href="/kurs">Kurs</a></li>
-                    <li><a href="/utstyr">Utstyr</a></li>
-                    <li><a href="/info">Info</a></li>
-                </ul>
-            </nav>
-        </div>
+        <header style={headerStyle}>
+            <img src="/eviguide_logo.png" alt="Logo" />
+            <ul style={navItemsStyle}>
+                <li><a href="/">Home</a></li>
+                <li><a href="/turer">Turer</a></li>
+                <li><a href="/kurs">Kurs</a></li>
+                <li><a href="/utstyr">Utstyr</a></li>
+                <li><a href="/info">Info</a></li>
+            </ul>
+        </header>
     );
 }
+
+const headerStyle: React.CSSProperties = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 50px',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    boxShadow: '0 3px 10px rgba(0, 0, 0, 0.1)'
+};
+
+const navItemsStyle: React.CSSProperties = {
+    listStyle: 'none',
+    display: 'flex',
+    gap: '20px'
+};
+
 
 export default Header;
