f5bba351884f2fa45e4b0f36b39109e0522f9777
Roger Schei Bergheim <rsbergheim@gmail.com>
2023-12-24 00:22:53 +0100

    Legger språkvalg i localstorage

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -1,24 +1,38 @@
-import React, { createContext, useContext, useState } from 'react';
-import { Language } from '../localization/Language';
+import React, {createContext, useContext, useState} from 'react';
+import {Language} from '../localization/Language';
 
 export const LanguageContext = createContext({
-  language: Language.en, // Initialize with a default value from the enum
-  setLanguage: (lang: Language) => {}
+    language: Language.no, // Initialize with a default value from the enum
+    setLanguage: (language: Language) => {}
 });
 
 export const useLanguage = () => useContext(LanguageContext);
 
-// Define the type for the props
 interface LanguageProviderProps {
-    children: React.ReactNode; // Define the children prop type
-  }
+    children: React.ReactNode;
+}
 
-export const LanguageProvider: React.FC<LanguageProviderProps> = ({ children }) => {
-    const [language, setLanguage] = useState(Language.no);
+export const LanguageProvider: React.FC<LanguageProviderProps> = ({children}) => {
+    // Attempt to get the preferred language from local storage
+    const storedLanguage = localStorage.getItem('preferredLanguage');
+
+    // Check if the stored language is part of the Language enum
+    const isStoredLanguageValid = Object.values(Language).includes(storedLanguage as Language);
+
+    // Use the stored language if it's valid, otherwise default to Language.no
+    const [language, setLanguage] = useState<Language>(
+        isStoredLanguageValid ? (storedLanguage as Language) : Language.no
+    );
+
+    // Update local storage whenever the language is set
+    const handleSetLanguage = (language: Language) => {
+        setLanguage(language);
+        localStorage.setItem('preferredLanguage', language);
+    };
 
     return (
-      <LanguageContext.Provider value={{ language, setLanguage }}>
-        {children}
-      </LanguageContext.Provider>
+        <LanguageContext.Provider value={{language, setLanguage: handleSetLanguage}}>
+            {children}
+        </LanguageContext.Provider>
     );
 };
