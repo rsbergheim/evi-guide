078bda772dfd5841a73d524f82776c871dd23ba3
Roger Schei Bergheim <rsbergheim@gmail.com>
2023-12-25 01:08:43 +0100

    Styling

--- src/pages/Utstyr.tsx
+++ src/pages/Utstyr.tsx
@@ -10,18 +10,16 @@ const Utstyr = () => {
 
     return (
         <Page>
-            <div>
-                <div className="utstyr-container">
-                    <div className="utstyr">
-                        <div className="utstyr-text">
-                            <h2>Ski</h2>
-                            <p>{localizer(language, "eviskiDescription1")}</p>
-                            <p>{localizer(language, "eviskiDescription2")}</p>
-                            <p>{localizer(language, "eviskiDescription3")}</p>
-                            <a href="https://www.eviski.com">eviski.com</a>
-                        </div>
-                        <a href="https://www.eviski.com"><img src="/eviski_logo.png" alt="Evi ski"/></a>
+            <div className="utstyr-container">
+                <div className="utstyr">
+                    <div className="utstyr-text">
+                        <h2>Ski</h2>
+                        <p>{localizer(language, "eviskiDescription1")}</p>
+                        <p>{localizer(language, "eviskiDescription2")}</p>
+                        <p>{localizer(language, "eviskiDescription3")}</p>
+                        <a href="https://www.eviski.com">eviski.com</a>
                     </div>
+                    <a href="https://www.eviski.com"><img src="/eviski_logo.png" alt="Evi ski"/></a>
                 </div>
             </div>
         </Page>
